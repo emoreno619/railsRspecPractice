e21b6245a6c2a5a6e0044905a757435ace5b9440
Eduardo Moreno <emoreno@uchicago.edu>
2015-07-16 10:17:45 -0700

    includes email tests and validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150716170124) do
+ActiveRecord::Schema.define(version: 20150716171535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150716170124) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "username"
+    t.string   "email"
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,8 +8,18 @@ describe User do
 
 	describe 'username' do
 		it 'cannot be blank' do
-			user = User.create(username:"")
+			invalid_user = User.create(username:"")
+
+			expect(invalid_user).to_not be_valid
 		end
 	end
 
+	describe 'email' do
+		it 'must be present' do
+			invalid_user = User.create(username:"Matthew", email:"")
+			valid_user = User.create(username:"Kermit the Frog", email:"ktf@henson.com")
+			expect(invalid_user).to_not be_valid
+			expect(valid_user).to be_valid
+		end
+	end
 end
